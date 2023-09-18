907d87f109e95556a33dea474ff7a4364da5f4b1
Cengomer <ca-app-pub-3725921328240837/9585494445>
2023-09-18 14:45:21 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by omer elmas on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
